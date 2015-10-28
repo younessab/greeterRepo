a9597bbe6ebee355533743c1b8b2d861457e9b9c
younes <younes.ayeb@gmail.com>
2015-10-28 11:46:40 +0100

    Modifying greeter.java
    Signed-off-by: younes <younes.ayeb@gmail.com>

--- greeter/src/main/java/edu/esprit/greeter/Greeter.java
+++ greeter/src/main/java/edu/esprit/greeter/Greeter.java
@@ -1,6 +1,15 @@
 package edu.esprit.greeter;
 
 public class Greeter {
+	
+	
+	/**
+	 * 
+	 * sayHello method
+	 * @param name
+	 * @return "Hello " + name
+	 */
+	
 	public String sayHello(String name) {
 			if(name.equals("MAN"))
 				throw new IllegalArgumentException();
